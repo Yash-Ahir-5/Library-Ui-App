c52a02fc8a664a439f2235cdf6e18fc97d7643c0
Yash Ahir <163372242+Yash-Ahir-5@users.noreply.github.com>
2024-04-03 00:38:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Insert from './Insert_Books';
 import Library from './Search_Books';
 import { Routes, Route, BrowserRouter as Router  } from 'react-router-dom';
+import Update from './Update_Books';
+// import Demo from './Demo';
 
 function App() {
   return (
     <Router>
     <Routes>
+      {/* <Route path="/" element={<Demo />} /> */}
       <Route path="/" element={<Library />} />
       <Route path="insert" element={<Insert />} />
-      {/* <Route path="insert" element={<Insert />} /> */}
+      <Route path="update/:id" element={<Update />} />
       {/* <Route path="contact" element={<Contact />} />
       <Route path="*" element={<NoPage />} /> */}
     </Routes>
